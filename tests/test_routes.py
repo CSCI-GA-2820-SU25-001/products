327fd0bf56d2537a411a9b007948ff351ad30c12
aditya-rajpathak <adityarajpathak07@gmail.com>
2025-06-21 03:34:52 +0000

    my commit

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -95,6 +95,28 @@ class TestProduct(TestCase):
         new_product = response.get_json()
         self.assertEqual(new_product["name"], test_product.name)
 
+    # ----------------------------------------------------------
+    # TEST UPDATE
+    # ----------------------------------------------------------
+    def test_update_product(self):
+        """It should Update an existing Product"""
+        # create a product to update
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED,
+            "Could not create test product",
+        ),
+        # update the product
+        new_product = response.get_json()
+        logging.debug(new_product)
+        new_product["description"] = "unknown"
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product["description"], "unknown")
+
         # TODO: Uncomment this code when get_accounts() is implemented
         # Check that the location header was correct
         # response = self.client.get(location)
