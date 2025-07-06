7c43e9dff82d84ff104f718e57c0a3e9b8915541
aditya-rajpathak. <adityarajpathak07@gmail.com>
2025-07-06 15:45:32 -0400

    Actions Endpoints

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -266,6 +266,13 @@ class TestProduct(TestCase):
         logging.debug("Response data = %s", data)
         self.assertIn("was not found", data["message"])
 
+    def test_purchase_not_found(self):
+        """It should not Purchase a Product that is not found"""
+        response = self.client.put(f"{BASE_URL}/0/purchase")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        data = response.get_json()
+        logging.debug("Response data = %s", data)
+        self.assertIn("was not found", data["message"])
 
 ######################################################################
 #  T E S T   S A D   P A T H S
