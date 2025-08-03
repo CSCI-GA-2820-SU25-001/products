bd8f154f5410b8a0ecc0a9889339d6a2cc02bcea
marielou27 <pml.marielou@gmail.com>
2025-08-03 23:01:11 +0000

    pytest-done

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -18,7 +18,7 @@ Module: error_handlers
 """
 from flask import current_app as app
 from service.routes import api
-from service.models import DataValidationError, DatabaseConnectionError
+from service.models import DataValidationError
 from . import status
 
 
@@ -35,15 +35,3 @@ def request_validation_error(error):
         "error": "Bad Request",
         "message": message,
     }, status.HTTP_400_BAD_REQUEST
-
-
-@api.errorhandler(DatabaseConnectionError)
-def database_connection_error(error):
-    """Handles Database Errors from connection attempts"""
-    message = str(error)
-    app.logger.critical(message)
-    return {
-        "status_code": status.HTTP_503_SERVICE_UNAVAILABLE,
-        "error": "Service Unavailable",
-        "message": message,
-    }, status.HTTP_503_SERVICE_UNAVAILABLE
